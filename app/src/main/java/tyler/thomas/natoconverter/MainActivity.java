956df3bb1e98d626fa51d932b1915ab07f73ff0c
Tyler <n01344909@humbermail.ca>
2021-07-24 19:39:41 -0400

    made ui cleaner and added some logic

--- app/src/main/java/tyler/thomas/natoconverter/MainActivity.java
+++ app/src/main/java/tyler/thomas/natoconverter/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,18 +36,21 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textViewOne);
         button = findViewById(R.id.buttonOne);
         view = findViewById(R.id.snackbar_action);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {//Checking if edit text field is left empty.
-                if (editText.getText().toString().equals("")){
-                    Snackbar.make(view, R.string.text_label, Snackbar.LENGTH_SHORT).show();
-                }//Checking if editText field is numeric.
-                else if(editText.getText().toString()){
-
-                }
-                else {
-                    textView.setText(NATOConverter(""+ editText.getText().toString()));
-                }
+        button.setOnClickListener(v -> {
+            if (editText.getText().toString().equals("")){
+                Snackbar.make(view, R.string.text_label, Snackbar.LENGTH_SHORT)
+                        .show();
+                textView.setText("");
+            }
+            else if(editText.getText().toString().matches(".*[0-9].*")){
+                Snackbar.make(view, R.string.text_number, Snackbar.LENGTH_SHORT)
+                        .show();
+                textView.setText("");
+            }
+            else {
+                Snackbar.make(view, R.string.done, Snackbar.LENGTH_SHORT)
+                        .show();
+                textView.setText(NATOConverter("" + editText.getText().toString()));
             }
         });
     }
@@ -56,16 +60,15 @@ public class MainActivity extends AppCompatActivity {
             "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey",
             "Xray", "Yankee", "Zulu")
             .collect(Collectors.toMap(word -> word.charAt(0), Function.identity()));
-    @RequiresApi(api = Build.VERSION_CODES.N)
+
     public static String NATOConverter(String name){
         return name.toUpperCase().chars()
                 .mapToObj(c -> WORDS.get((char)c))
                 .filter(Objects::nonNull)
                 .collect(Collectors.joining(" "));
     }
-    //Alert Dialog code
     public void onBackPressed() {
-        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Exit?")
                 .setCancelable(false)
                 .setPositiveButton("Yes", (dialog, k) -> MainActivity.super.onBackPressed())
